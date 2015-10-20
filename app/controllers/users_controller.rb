ba065a23c718b0b926dfd6f1fe6977fac032182f
truongkma <nguyendactruong.kma@gmail.com>
2015-10-20 17:25:23 +0700

    42806 sign-up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "flashs.user.signup"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation, :is_supervisor
+  end
+end
