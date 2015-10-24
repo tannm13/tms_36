3d15031caaee571af647dd1003a93e74a199cf56
khanhnv3007 <khanhnv3007@gmail.com>
2015-10-24 23:29:57 +0700

    Create courses

--- app/controllers/supervisors/dashboard_controller.rb
+++ app/controllers/supervisors/dashboard_controller.rb
@@ -2,6 +2,7 @@ class Supervisors::DashboardController < ApplicationController
   before_action :authorize_user
   before_action :authorize_supervisor
   def index
-    @number_of_users = User.count
+    @number_of_users = User.size
+    @number_of_courses = Course.size
   end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,11 @@
 class Course < ActiveRecord::Base
+  enum status: {started: 0, finished: 1}
   has_many :user_courses
   has_many :users, through: :user_courses
   has_many :course_subjects
   has_many :subjects, through: :course_subjects
+
+  validates :name, presence: true, length: {maximum: 50}
+  validates :description, presence: true, length: {maximum: 1000}
+  validates :status, presence: true
 end
