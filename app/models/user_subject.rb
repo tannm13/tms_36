a64c86d717d4277318ad8d9a73b1ef49c0248b95
tannm13 <tannm132@gmail.com>
2015-11-10 11:42:29 +0700

    fixbug-42561-add-trainees

--- app/models/user_subject.rb
+++ app/models/user_subject.rb
@@ -8,11 +8,22 @@ class UserSubject < ActiveRecord::Base
   belongs_to :user_course
   has_many :user_tasks
 
+  after_create :create_user_tasks
   after_update :log_activity
 
+  private
   def finish_subject
     if self.update_status :finished
       user_tasks.each{|user_task| user_task.update_status :finished}
+    end
+  end
+
+  def create_user_tasks
+    if self.subject.started? && self.subject.tasks.present?
+      self.subject.tasks.each do |task|
+        self.user_tasks.create(user_id: self.user.id, task_id: task.id,
+          status: :started)
+      end
     end
   end
 end
