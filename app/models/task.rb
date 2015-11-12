935d7a89c7d9e3df1a47fd3824d718541aa72d88
tannm13 <tannm132@gmail.com>
2015-11-12 10:20:03 +0700

    fixbug-42550-view-activity

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,4 +7,14 @@ class Task < ActiveRecord::Base
 
   has_many :user_tasks, dependent: :destroy
   has_many :users, through: :user_tasks
+
+  after_create :create_user_tasks
+
+  private
+  def create_user_tasks
+    self.subject.user_subjects.each do |user_subject|
+      user_subject.user_tasks.create(user_id: user_subject.user.id,
+        task_id: self.id, status: self.subject.status)
+    end
+  end
 end
