3e84f7d21fe3d7cdef3273c32baa3edbf5b33020
tannm13 <tannm132@gmail.com>
2015-11-16 12:45:05 +0700

    fixbug-edit-subject

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -6,6 +6,7 @@ class Subject < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
   has_many :course_subjects, dependent: :destroy
   has_many :courses, through: :course_subjects
+  has_many :user_subjects, dependent: :destroy, inverse_of: :subject
 
   scope :recent, ->{order created_at: :desc}
 
--- app/models/user_subject.rb
+++ app/models/user_subject.rb
@@ -6,7 +6,7 @@ class UserSubject < ActiveRecord::Base
   belongs_to :user
   belongs_to :subject
   belongs_to :user_course
-  has_many :user_tasks
+  has_many :user_tasks, dependent: :destroy
 
   after_create :create_user_tasks
   after_update :log_activity
